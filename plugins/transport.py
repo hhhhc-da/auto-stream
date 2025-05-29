ed81450484b33aa6beedc3c6000efa06d74f30bf
unknown <zhengbindesu@gmail.com>
2025-05-29 22:17:37 +0800

    Nanoka edit. 2025/5/29

---
+++ plugins/transport.py
@@ -0,0 +1,96 @@
+#coding=utf-8
+import blcsdk
+from blcsdk import client as cli
+from blcsdk import models
+from typing import Optional
+import sys
+import asyncio
+import argparse
+import requests
+
+'''
+每次启动时，一定要修改 blcsdk/api.py 中的 BLC_PORT 和 BLC_TOKEN 环境变量
+
+os.environ['BLC_PORT'] = '12450'
+os.environ['BLC_TOKEN'] = 'FaeDaD7bCa7895F5b9afE509aAB0cAdB'
+
+否则连接不上服务器，每次都不同，最终软件需要编译成插件放入 blivechat/data/plugins 目录下
+'''
+
+# 全局变量，用于控制插件的关闭事件
+shut_down_event: Optional[asyncio.Event] = None
+
+class MsgHandler(blcsdk.BaseHandler):
+    '''
+    消息处理类，作为附件加入到 blivechat SDK 中
+    '''
+    def __init__(self, options: Optional[argparse.Namespace] = None):
+        super().__init__()
+        
+        self.options = options
+        
+        
+    def _on_add_text(self, client: cli.BlcPluginClient, message: models.AddTextMsg, extra: models.ExtraData):
+        """收到弹幕"""
+        print(f'Received text message: {message.content}')
+        
+        # 将弹幕发送到指定的服务器
+        if self.options and self.options.url:
+            try:
+                response = requests.post(self.options.url, json={'content': message.content})
+                if response.status_code == 200:
+                    print('弹幕发送成功')
+                else:
+                    print(f'弹幕发送失败: {response.status_code} - {response.text}')
+            except requests.RequestException as e:
+                print(f'弹幕发送异常: {e}')
+        else:
+            print('未配置服务器地址，无法发送弹幕')
+        
+def opt_parser():
+    """
+    命令行参数解析器
+    """
+    parser = argparse.ArgumentParser(description='基于 BliveChat SDK 开发的弹幕发送插件')
+    parser.add_argument('--url', type=str, default='http://localhost:83/', help='需要将弹幕发送到的服务器地址')
+    return parser.parse_args()
+        
+    
+async def init_sdk(options: Optional[argparse.Namespace] = None):
+    """
+    初始化blivechat SDK
+    """
+    print('blivechat SDK init.')
+    await blcsdk.init()
+    
+    if not blcsdk.is_sdk_version_compatible():
+        raise RuntimeError('SDK version is not compatible')
+    
+    _msg_handler = MsgHandler(options=options)
+    blcsdk.set_msg_handler(_msg_handler)
+    
+async def main(options: Optional[argparse.Namespace] = None) -> int:
+    '''
+    主程序入口
+    '''
+    print('blivechat plugin start.')
+    
+    try:
+        await init_sdk(options=options)
+    except Exception as e:
+        print(f'blivechat plugin init failed: {e}')
+        sys.exit(-1)
+    
+    print('blivechat plugin init success.')
+    
+    # 作为一个插件，阻塞进程即可
+    global shut_down_event
+    shut_down_event = asyncio.Event()
+    await shut_down_event.wait()
+    
+    return 0
+    
+    
+if __name__ == '__main__':
+    opt = opt_parser()
+    sys.exit(asyncio.run(main()))
--- start.bat
+++ start.bat
@@ -1,9 +1,9 @@
 @echo off
 
-rem 启动STT服务（在新窗口中运行）
 start "" stt_service.exe
 
-rem 激活conda环境并启动Python服务（在新窗口中运行）
 start "" cmd /k "conda activate chat && python modules\llm_server.py"
+
+start "" blivechat\blivechat.exe
 
 echo 已启动所有服务
